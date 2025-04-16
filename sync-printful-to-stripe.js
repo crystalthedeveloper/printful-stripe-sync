593fa0dd5a727eceace9cf889568d1c02370b0c7
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2025-04-16 14:48:06 -0400

    Printful to Stripe price v3

--- sync-printful-to-stripe.js
+++ sync-printful-to-stripe.js
@@ -6,12 +6,12 @@ import Stripe from "stripe";
 import fetch from "node-fetch";
 dotenv.config();
 
-const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+const STRIPE_SECRET_TEST = process.env.STRIPE_SECRET_TEST;
 const PRINTFUL_API_KEY = process.env.PRINTFUL_API_KEY;
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-const stripe = new Stripe(STRIPE_SECRET_KEY, { apiVersion: "2023-10-16" });
+const stripe = new Stripe(STRIPE_SECRET_TEST, { apiVersion: "2023-10-16" });
 
 async function sync() {
   try {
